62e060c00547258ac70e37ada0a9361a1d688588
Rahul Gautam <deltaman2000@gmail.com>
2017-11-01 13:08:50 -0500

    Initial Push

---
+++ Data Preprocessing/practice_data_preprocessing_template.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Nov  1 12:46:31 2017
+
+@author: Deltaman
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+
+
